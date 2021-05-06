bc324e68c8ab018c48388ed4b329151bd21e7305
Shape Shapa <Swiveli.production@gmail.com>
2021-05-06 19:21:21 +0700

    feature:(404 page)
    added a 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import styles from '../styles/Home.module.scss'
+
+export default function FourOhFour(): JSX.Element {
+  return (
+    <div className={styles.container}>
+      <h1>404</h1>
+      <Link href="/">
+        <a>Return to home</a>
+      </Link>
+    </div>
+  )
+}
